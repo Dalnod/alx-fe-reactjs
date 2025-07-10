3ead26371a8fe0b608d76f80691ed0fdcefbcb25
Dalnod <donerharhine@gmail.com>
2025-07-10 20:28:04 +0100

    updated

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,15 @@
 import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header"
+import MainContent from "./components/MainContent"
+import Footer from "./components/Footer"
 
 export default function App () {
     return (
         <>
             <WelcomeMessage />
+            <Header />
+            <MainContent />
+            <Footer />
         </>
     )
 };

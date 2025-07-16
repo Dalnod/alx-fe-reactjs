ba25a4fd5b2a25232ff59d3488684e59472090b8
Dalnod <donerharhine@gmail.com>
2025-07-16 16:16:07 +0100

    name changed

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+            <p>&copy; 2023 City Lovers</p>
+        </footer>
+    );
+}

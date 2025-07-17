680faa7b795db92a10dcea08d9e48815d08d9e51
Dalnod <donerharhine@gmail.com>
2025-07-17 01:07:17 +0100

    updated

--- alx-react-app/src/components/MainContent.jsx
+++
@@ -1,7 +0,0 @@
-export default function MainContent() {
-    return (
-        <main style={{display: 'flex',  justifyContent: 'center'}}>
-            <p>I love to visit New York, Paris, and Tokyo.</p>
-        </main>
-    );
-}

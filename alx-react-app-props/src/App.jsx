92994dacd84a369ecd2ca926c93988d18221c60a
Dalnod <donerharhine@gmail.com>
2025-07-20 15:46:09 +0100

    updated

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,19 +1,9 @@
-import WelcomeMessage from "./components/WelcomeMessage";
-import Header from "./components/Header"
-import MainContent from "./components/MainContent"
-import Footer from "./components/Footer"
-import UserProfile from "./components/UserProfile";
-import Counter from "./components/Counter";
+import ProfilePage from './ProfilePage';
 
-export default function App () {
-    return (
-        <>
-           {/* <WelcomeMessage />
-            <Header />
-            <MainContent />
-            <Footer />
-            <UserProfile name="Alice" age="25" bio="Loves hiking and photography"  /> */}
-            <Counter />
-        </>
-    )
-};
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return <ProfilePage userData={userData} />;
+}
+
+export default App;
--- alx-react-app-props/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-            <p>&copy; 2023 City Lovers</p>
-        </footer>
-    );
-}

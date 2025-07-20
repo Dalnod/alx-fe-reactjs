5578000b261ffad50f92dad9e4d310ce595c9334
Dalnod <donerharhine@gmail.com>
2025-07-20 19:52:56 +0100

    updated

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,9 +1,15 @@
+import UserContext from './UserContext';
 import ProfilePage from './ProfilePage';
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const userData = {Phone: "09079713507", name: "Erharhine Donald", email: "donerharhine@gmail.com"
+   };
 
-  return <ProfilePage userData={userData} />;
+  return (
+    <UserContext.Provider value = {userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  ); 
 }
 
-export default App;
+export default App; 

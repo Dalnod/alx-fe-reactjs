f6c13d2c0d94b68acd7918327e07555fca2fcdcb
Dalnod <donerharhine@gmail.com>
2025-07-27 15:20:30 +0100

    updatd

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,16 +1,18 @@
 import { useRecipeStore } from './recipeStore';
 
-  const RecipeList = () => {
-    const recipes = useRecipeStore(state => state.recipes);
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes); // Get recipes from store
 
-    return (
-      <div>
-        {recipes.map(recipe => (
-          <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-          </div>
-        ))}
-      </div>
-    );
-  };
+  return (
+    <div>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,7 +1,10 @@
-import create from 'zustand'
+import { create } from 'zustand';
 
-const useRecipeStore = create(set => ({
-  recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
+const useRecipeStore = create((set) => ({
+  recipes: [], // Initial state: empty array of recipes
+  addRecipe: (newRecipe) => 
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })), // Add new recipe
+  setRecipes: (recipes) => set({ recipes }), // Overwrite entire recipes array
 }));
+
+export default useRecipeStore;

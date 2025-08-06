671033dfee8f18c4e3149b8098f285b58b4f804b
Dalnod <donerharhine@gmail.com>
2025-08-06 12:14:45 +0100

    updated

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,8 +4,8 @@ function UserProfile() {
     let styles = '';
 
   return (
-    <div className="bg-gray-100 sm:p-4 mx-auto my-20 rounded-lg shadow-lg  md:p-8 max-w-sm ">
-      <img className="rounded-full w-24 h-24 mx-auto md:h-36 md:w-36" src="https://via.placeholder.com/150" alt="User" />
+    <div className="bg-gray-100 sm:p-4 max-w-xs mx-auto my-20 rounded-lg shadow-lg  md:p-8 md:max-w-sm ">
+      <img className="rounded-full sm:w-24 sm:h-24 mx-auto md:h-36 md:w-36" src="https://via.placeholder.com/150" alt="User" />
       <h1 className="text-lg text-blue-800 my-4 md:text-xl ">John Doe</h1>
       <p className="text-gray-600 text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>

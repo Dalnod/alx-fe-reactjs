149c5987a53e4d17760e44e74a8340a67d30f4ef
Dalnod <donerharhine@gmail.com>
2025-08-12 16:46:15 +0100

    updated

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+       <h1 className='text-4xl font-semibold text-blue-900'>Sharp!!</h1> 
+      </div>   
+    </>
+  )
+}
+
+export default App
